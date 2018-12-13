4ae1a2bd799a3430920423f5c5c98fd938129186
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2018-12-13 18:27:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '!'
- 
- 
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Top Server  `,"https://www.twitch.tv/dggamingbot")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+client.on('ready',async () => { client.channels.find(ch => ch.id === "517598553502121986" && ch.type === 'voice').join(); });
  
  
  
