a601fe1fa74b938506b2255a7fca7c3c1eb51af5
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2018-12-13 21:24:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-'
- 
- 
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TopServer  `,"https://www.twitch.tv/dggamingbot")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.on('ready',async () => {
+
+let channel = client.channels.get("517597955616800768");
+if(channel.type !== 'voice') throw new Error("# Error : The channel must be a voice channel.");
+channel.join();
+
+setInterval(() => {
+if(!channel.guild.member(client.user).voiceChannel) {
+channel.join();
+}
+}, 100);
 });
  
  
