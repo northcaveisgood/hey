16808209f3584a05d536b926c46b590eee66908d
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2018-12-13 18:39:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,26 @@
 client.on('ready',async () => { client.channels.find(ch => ch.id === "517598553502121986" && ch.type === 'voice').join(); });
- 
+   console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
  
  
 client.login(process.env.BOT_TOKEN);
