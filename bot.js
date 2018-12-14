3d47b4638271dd53a6052fd076732e02336f4072
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2018-12-14 07:47:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-st'
- 
- 
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`OBX SERVER  `,"https://www.twitch.tv/dggamingbot")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "sf")) {
+        let embed = new Discord.RichEmbed()
+        .addField(`:id: Server ID:`, `${message.guild.id}`, true)
+        .addField(`:calendar: Created on:`, `${moment(message.guild.createdAt).format(`D/M/YYYY h:mm`)} \n ${moment(message.guild.createdAt).locale("AR-eg").fromNow()}`,true)
+        .addField(`:crown: Owned by`, `${message.guild.owner.user.tag} [${message.guild.owner.user.id}]`,true) 
+        .addField(`:busts_in_silhouette: Members [${message.guild.members.size}]`, `**${message.guild.members.filter(c => c.presence.status !== "offline").size}** Online`, true)
+        .addField(`:speech_balloon: Channels [${message.guild.channels.size}]`,`**${message.guild.channels.filter(f => f.type === "text").size}** Text | **${message.guild.channels.filter(f => f.type === "voice").size}** Voice`,true)
+        .addField(`:earth_africa: Others`, `**Region:** ${message.guild.region} \n **Verification level:** ${message.guild.verificationLevel}`, true)  
+        .addField(`:closed_lock_with_key: Roles [${message.guild.roles.size}]`, `To see the whole list with all roles use **#roles**`, true) 
+        
+        // .setThumbnail(`${message.guild.iconURL}`)
+        .setColor(`black`)
+        .setAuthor(`${message.guild.name}`, `${message.guild.iconURL}`)
+       
+    message.channel.sendEmbed(embed)
+    }
 });
  
  
