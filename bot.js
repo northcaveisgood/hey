faa921a31dee2a4fe329bc4a0c235b032da430fa
northcaveisgood <45044753+northcaveisgood@users.noreply.github.com>
2018-12-14 07:09:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,33 @@
-client.on('ready',async () => {
-
-let channel = client.channels.get("517597955616800768");
-if(channel.type !== 'voice') throw new Error("# Error : The channel must be a voice channel.");
-channel.join();
-
-setInterval(() => {
-if(!channel.guild.member(client.user).voiceChannel) {
-channel.join();
-}
-}, 100);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'البرفيكس حق البوت'
+ 
+ 
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`LastCodes4Ever  `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
  
  
